437dbaa656920e2bd861ac9c5707c2e635e7b911
Misagh Moayyed <mmoayyed@unicon.net>
2015-11-03 15:57:33 -0700

    removed bad package name

--- idp-oidc-impl/src/main/java/net/shibboleth/idp/oidc/flow/PreAuthorizeUserApprovalAction.java
+++ idp-oidc-impl/src/main/java/net/shibboleth/idp/oidc/flow/PreAuthorizeUserApprovalAction.java
@@ -43,7 +43,6 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * Prepares the webflow response for the approval/consent view.
