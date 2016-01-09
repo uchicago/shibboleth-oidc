b7b3e823e2248282dacda45c1a65332532aa6afd
Misagh Moayyed <mmoayyed@unicon.net>
2016-01-09 02:55:15 -0700

    working on endpoint refactoring
    # Conflicts:
    #	idp-webapp-overlay/src/main/webapp/idp/conf/logback.xml

---
+++ idp-oidc-impl/src/main/java/net/shibboleth/idp/oidc/config/ShibbolethOAuth2RequestFactory.java
@@ -0,0 +1,33 @@
+package net.shibboleth.idp.oidc.config;
+
+import org.mitre.oauth2.service.ClientDetailsEntityService;
+import org.mitre.openid.connect.request.ConnectOAuth2RequestFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.oauth2.provider.AuthorizationRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * This is {@link ShibbolethOAuth2RequestFactory}.
+ */
+@Service("shibbolethOAuth2RequestFactory")
+@Primary
+public class ShibbolethOAuth2RequestFactory extends ConnectOAuth2RequestFactory {
+    @Autowired
+    public ShibbolethOAuth2RequestFactory(final ClientDetailsEntityService clientDetailsService) {
+        super(clientDetailsService);
+    }
+
+    @Override
+    public AuthorizationRequest createAuthorizationRequest(final Map<String, String> inputParams) {
+        final AuthorizationRequest request = super.createAuthorizationRequest(inputParams);
+        if (request.getResponseTypes().isEmpty()) {
+            request.setResponseTypes(Collections.singleton("code"));
+        }
+
+        return request;
+    }
+}
