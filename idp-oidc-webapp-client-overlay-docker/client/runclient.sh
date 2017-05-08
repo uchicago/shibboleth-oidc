31741c6ce1ac7b1d839c2ac2b6ef8f6addbfb2d7
Misagh Moayyed <mmoayyed@unicon.net>
2017-05-08 10:58:04 -0700

    Initial pass at the docker build

--- idp-oidc-webapp-client-overlay-docker/client/runclient.sh
+++ idp-oidc-webapp-client-overlay-docker/client/runclient.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
+
+export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+
+echo "**************************"
+echo "JAVA_HOME: $JAVA_HOME"
+echo "*******************************"
+
+echo "Exporting certificate"
+keytool -export -file idptestbedhttpdproxy -keystore /etc/jetty/thekeystore -alias idptestbedhttpdproxy
+
+echo "Importing certificate"
+keytool -import -file idptestbedhttpdproxy -keystore $JAVA_HOME/jre/lib/security/cacerts -alias idptestbed -storepass changeit  -noprompt
+
 ./mvnw clean package jetty:run-forked
